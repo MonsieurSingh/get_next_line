1edc90b4de7c5d8a6c60a9b9831c7a28dbda4148
Teghjyot Singh <teghjyot@icloud.com>
2024-03-11 01:03:21 +1030

    Initial Commit

---
+++ get_next_line/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  get_next_line
+//
+//  Created by Devjyot Singh on 11/3/2024.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	printf("Hello, World!\n");
+	return 0;
+}
